391fa21b0b0ef9b0dd8802d68349511911c02d83
LewisH95 <c3057481@newcastle.ac.uk>
2024-07-06 12:34:57 +0100

    First Commit

--- src/main/java/com/example/adaptivequizbackend/AdaptiveQuizBackEndApplication.java
+++ src/main/java/com/example/adaptivequizbackend/AdaptiveQuizBackEndApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdaptiveQuizBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdaptiveQuizBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptiveQuizBackEndApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/adaptivequizbackend/service/UserService.java
+++ src/main/java/com/example/adaptivequizbackend/service/UserService.java
@@ -1,2 +1,32 @@
-package com.example.adaptivequizbackend.service;public class UserService {
+package com.example.adaptivequizbackend.service;
+
+import com.example.adaptivequizbackend.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+
+        User user = new User(1, "Dave", 30, "dave@mail.com");
+        userList.add(user);
+    }
+
+    public Optional<User> getUser(Integer id) {
+        Optional optional = Optional.empty();
+        for (User user : userList) {
+            if (id == user.getId()) {
+                optional = Optional.of(user);
+                return optional;
+            }
+        }
+        return optional;
+    }
 }
